34842af250332a9b923486a67a0e9462b0e6bb06
Angelo Lara <angelolarajara@hotmail.com>
2022-02-15 11:15:24 -0300

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent implements OnInit {
+
+  heroes: string[] = ['spiderman', 'iroman', 'hulk', 'thor'];
+  heroeBorrado: string = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  borrarHeroe() {
+    console.log("Borrando...");
+    this.heroeBorrado = this.heroes.shift() || '';
+  }
+
+}
